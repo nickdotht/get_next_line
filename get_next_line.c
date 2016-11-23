7efcb831e88886180d0c8d3d0f1a385173f22a46
Nick Rameau <r4meau@gmail.com>
2016-11-23 06:11:39 -0800

    Code almost works, jut gott deal with EOF now

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jrameau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/29 22:52:30 by jrameau           #+#    #+#             */
-/*   Updated: 2016/11/16 21:04:25 by jrameau          ###   ########.fr       */
+/*   Updated: 2016/11/23 06:06:22 by jrameau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,17 @@ int		get_next_line(const int fd, char **line)
 	static char	*tmp_str;
 	char		*tmp_str2;
 
-	printf("TEST MY ASS");
+	/* ret = 0; */
 	line_complete = 0;
 	MALLCHECK((curr = ft_strnew(1)));
 	MALLCHECK((*line = ft_strnew(1)));
+	if (ret && ret < BUFF_SIZE && tmp_str && tmp_str[0] == '\0')
+	{
+		free(tmp_str);
+		return (0);
+	}
+
+	printf("ret is %d\n", ret);
 
 	/*
 	 * If tmp_str has characters in it from previous reading
@@ -34,25 +41,28 @@ int		get_next_line(const int fd, char **line)
 	 * the newline to curr and save the part that comes after it
 	 * back into tmp_str for later use.
 	*/
-	if (tmp_str && ft_strlen(tmp_str))
+	if (tmp_str && tmp_str[0] != '\0')
 	{
 		if (ft_strchr(tmp_str, '\n') == NULL)
 		{
 			MALLCHECK((curr = ft_strjoin(curr, tmp_str)));
+			MALLCHECK((tmp_str = ft_strnew(1)));
+			/*
+			 * Find a way to set the line as complete here
+			 * if done reading the file
+			*/
 		}
 		else
 		{
 			MALLCHECK((tmp_str2 = ft_strnew(1)));
 			i = 0;
 			while (tmp_str[i] && tmp_str[i] != '\n')
-				MALLCHECK((curr = ft_strjoinch(curr, tmp_str[i])));
+				MALLCHECK((curr = ft_strjoinch(curr, tmp_str[i++])));
 			if (tmp_str[i] == '\n')
 				line_complete = 1;
 			while (tmp_str[++i])
 				MALLCHECK((tmp_str2 = ft_strjoinch(tmp_str2, tmp_str[i])));
-			MALLCHECK((tmp_str = ft_strnew(1)));
-			MALLCHECK((tmp_str = ft_strjoin(tmp_str, tmp_str2)));
-			free(tmp_str2);
+			tmp_str = tmp_str2;
 		}
 	}
 
@@ -66,12 +76,11 @@ int		get_next_line(const int fd, char **line)
 	while (!line_complete && (ret = read(fd, buf, BUFF_SIZE)))
 	{
 		buf[ret] = '\0';
-
+		if (ret < BUFF_SIZE)
+			line_complete = 1;
 		if (ft_strchr(buf, '\n') == NULL)
 		{
 			MALLCHECK((curr = ft_strjoin(curr, buf)));
-			if (ret < BUFF_SIZE)
-				line_complete = 1;
 		}
 		else
 		{
@@ -79,24 +88,18 @@ int		get_next_line(const int fd, char **line)
 			i = 0;
 			while (buf[i] && buf[i] != '\n')
 				MALLCHECK((curr = ft_strjoinch(curr, buf[i++])));
-			if (buf[i] == '\n' || (!buf[i] && ret < BUFF_SIZE))
+			if (buf[i] == '\n' || (buf[i] == '\0' && ret < BUFF_SIZE))
 				line_complete = 1;
 			while (buf[++i])
 				MALLCHECK((tmp_str = ft_strjoinch(tmp_str, buf[i])));
 		}
 	}
-	printf("\nLINE%sLINEEND\n", curr);
 
+	/*
+	 * If the line is complete, add it to *line
+	*/
+	*line = curr;
 	if (line_complete)
-		MALLCHECK((*line = ft_strjoin(*line, curr)));
-	free(curr);
-	printf("Does this work?");
-	if (ret < BUFF_SIZE)
-	{
-		if (tmp_str)
-			free(tmp_str);
-		return (0);
-	}
-	printf("WTF DUDE!");
-	return (1);
+		return (1);
+	return (0);
 }
--- gnl.sh
+++ gnl.sh
@@ -1,5 +1,5 @@
 # make -C libft/ fclean && make -C libft/
-clang -Wall -Wextra -Werror -I libft/includes -o get_next_line.o -c get_next_line.c
-clang -Wall -Wextra -Werror -Ilibft/includes -o main.o -c main.c
-clang -o test_gnl -fsanitize=address main.o get_next_line.o -Ilibft/includes/ -Llibft/ -lft #-fsanitize=address
-./test_gnl m83.txt
+gcc -Wall -Wextra -Werror -I libft/includes -o get_next_line.o -c get_next_line.c
+gcc -Wall -Wextra -Werror -Ilibft/includes -o main.o -c gnl_main.c
+gcc -o test_gnl -fsanitize=address main.o get_next_line.o -Ilibft/includes/ -Llibft/ -lft #-fsanitize=address
+./test_gnl 10 3
