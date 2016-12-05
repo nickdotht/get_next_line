29bb2e4dcdbbbc60c2f9bf3341a1155e5905dc51
Nick Rameau <r4meau@gmail.com>
2016-12-05 08:24:47 -0800

    FUCK

--- get_next_line.c
+++ get_next_line.c
@@ -6,42 +6,48 @@
 /*   By: jrameau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/29 22:52:30 by jrameau           #+#    #+#             */
-/*   Updated: 2016/12/02 22:11:25 by jrameau          ###   ########.fr       */
+/*   Updated: 2016/12/02 20:14:46 by jrameau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+t_list	*get_correct_file(t_list *file, int fd)
+{
+	t_list		*tmp;
+
+	if (!file)
+	{
+		tmp = ft_lstnew(NULL, fd);
+	}
+	else
+	{
+		tmp = file;
+		while (tmp != NULL)
+		{
+			if ((int)tmp->content_size == fd)
+				return (tmp);
+			tmp = tmp->next;
+		}
+		tmp = ft_lstnew(NULL, fd);
+		tmp->next = file;
+	}
+	return (tmp);
+}
+
 int		get_next_line(const int fd, char **line)
 {
-	int						ret;
-	char					buf[BUFF_SIZE + 1];
-	static char		*file;
-	int						i;
+	int				ret;
+	char			buf[BUFF_SIZE + 1];
+	int				i;
+	static t_list	*file;
 
-	if (read(fd, buf, 0) < 0 || line == NULL)
+	if ((fd < 0 || line == NULL || read(fd, buf, 0) < 0))
 		return (-1);
-	if (!file || file[0] == '\0')
-		MALLCHECK((file = ft_strnew(1)));
+	file = get_correct_file(file, fd);
 	MALLCHECK((*line = ft_strnew(1)));
-	/*
-	 *	Read the whole file save it in a variable
-	 */
-	while ((ret = read(fd, buf, BUFF_SIZE)))
-	{
-		buf[ret] = '\0';
-		MALLCHECK((file = ft_strjoin(file, buf)));
-	}
-	/*
-	*		Check the file variable to know if it exists and has text in it
-	*		If it does, add everything from the start to the next \n or eol
-	*		If not, do nothing.
-	*/
-	if (file && file[0] != '\0')
-	{
-		i = ft_copyuntil(line, file, '\n');
-		file += i + 1;
-		return (1);
-	}
-	return (0);
+	
+	if (ret < BUFF_SIZE && !ft_strlen(file->content))
+		return (0);
+	return (1);
 }
