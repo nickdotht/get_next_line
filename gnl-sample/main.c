05acd004e0596692a66b7dfa5a3a2ced182f4b9e
Nick Rameau <r4meau@gmail.com>
2016-12-07 04:14:53 -0800

    Update custom test files

--- gnl-sample/main.c
+++ gnl-sample/main.c
@@ -6,7 +6,7 @@
 /*   By: jrameau <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/06 05:26:42 by jrameau           #+#    #+#             */
-/*   Updated: 2016/12/06 05:27:39 by jrameau          ###   ########.fr       */
+/*   Updated: 2016/12/07 04:14:14 by jrameau          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int		main(int argc, char **argv)
 		fd = open(argv[1], O_RDONLY);
 	else
 		return (2);
-	while (get_next_line(fd, &line))
+	while (get_next_line(fd, &line) == 1)
 	{
 		ft_putendl(line);
 		free(line);
